1f1022b155e6e74d76f99f4706e67ef0a76b59ec
user <user@teminal>
2019-04-28 17:02:32 -0500

    Starting

---
+++ src/poo/E01AppCuadrado/AppCuadrado.java
@@ -0,0 +1,38 @@
+package poo.E01AppCuadrado;
+
+/**
+ * Utiliza la clase Cuadrado
+ *
+ * @author Hernán
+ */
+public class AppCuadrado {
+
+    public static void main(String[] args) {
+        // Declarar dos variables de tipo Cuadrado
+        CCuadrado c1;
+        CCuadrado c2;
+
+        // Inicializar los cuadrados haciendo uso de sus constructores
+        c1 = new CCuadrado();
+        c2 = new CCuadrado(3.0d);
+        // Mostrar el valor de los lados de cada cuadrado
+        System.out.println("El lado de c1 es: " + c1.obtenerLado());
+        System.out.println("El lado de c2 es: " + c2.obtenerLado());
+        System.out.println();
+        // Cambiar el valor del lado de c1 y mostrarlo
+        c1.modificarLado(5.0d);
+        System.out.println("Cambiando el lado de c1...");
+        System.out.println("El lado de c1 es: " + c1.obtenerLado());
+        System.out.println();
+        // Mostrar el area de c1 y el perimetro de c2
+        System.out.println("El area de c1 es: " + c1.area());
+        System.out.println("El perímetro de c2 es: " + c2.perimetro());
+        // Copiando un Cuadrado;
+        CCuadrado c3;
+        c3 = c1;
+        System.out.println("Lado c3 = " + c3.obtenerLado());
+        System.out.println("Perímetro c3 = " + c3.perimetro());
+        System.out.println("Area c3= " + c3.area());
+    }
+
+}
---
+++ src/poo/E01AppCuadrado/CCuadrado.java
@@ -0,0 +1,36 @@
+package poo.E01AppCuadrado;
+
+/**
+ * Clase Cuadrado que representa una figura geométrica de cuatro lados iguales
+ * @author Hernán Nina Hanco
+ */
+public class CCuadrado {
+    //================== ATRIBUTOS O CARACTERÍSTICAS ==================
+    private double aLado;
+    // =============== MÉTODOS U OPERACIONES
+    // Constructores
+    public CCuadrado(){
+        aLado = 0;
+    }
+    public CCuadrado(double pLado){
+        aLado = pLado>=0?pLado:0;
+    }
+    // Modificadores 
+    public void modificarLado(double pLado){
+        aLado = pLado>=0?pLado:0;
+    }
+    // Seleectores o Accesadores
+    public double obtenerLado(){
+        
+        return aLado;
+    }
+    // Otras operaciones
+    public double perimetro(){
+        return (4*aLado);
+    }
+    // -----------
+    public double area()
+    {
+        return Math.pow(aLado, 2);
+    }
+}
